5025b530a3648febf29b963789748b63faa746b5
SoloGuardiaN <44646589+SoloGuardiaN@users.noreply.github.com>
2018-10-31 10:33:20 -0500

    Default files.

---
+++ Project1.java
@@ -0,0 +1,23 @@
+/* Java Graphics Project  
+ * Major Grade
+ */
+
+import java.awt.*;
+import java.applet.*;
+import java.io.IOException.*;
+import java.net.*;
+
+public class Project1 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		// add your code here	
+
+   
+	
+	}	
+	
+	
+}
+
+
